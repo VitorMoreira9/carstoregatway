21146ce9746b43d0d46fbf1c33b34bd8a4c13195
VitorMoreira9 <vitorcarmovitor@gmail.com>
2026-04-22 19:07:37 -0300

    [master] - feat: ajuste

---
+++ src/main/java/br/projetopessoal/carstoregateway/CarstoregatewayApplication.java
@@ -0,0 +1,13 @@
+package br.projetopessoal.carstoregateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarstoregatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarstoregatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/projetopessoal/carstoregateway/CarstoregatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.projetopessoal.carstoregateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarstoregatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
